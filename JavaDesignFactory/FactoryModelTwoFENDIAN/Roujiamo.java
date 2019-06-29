afcb000290534da2952f7dad20bc53e0f4b3452e
kai123wen <1819306561@qq.com>
2019-06-29 09:50:13 +0800

    java工厂模式

---
+++ JavaDesignFactory/FactoryModelTwoFENDIAN/Roujiamo.java
@@ -0,0 +1,8 @@
+package FactoryModelTwoFENDIAN;
+
+public abstract class Roujiamo {
+	protected String name;
+	public void makeRoujiamo() {
+		System.out.println("ÖÆ×÷"+ name);
+	}
+}
---
+++ JavaDesignFactory/RoujiamoSuan.java
@@ -0,0 +1,6 @@
+
+public class RoujiamoSuan extends Roujiamo{
+	public RoujiamoSuan(String name) {
+		this.name = name;
+	}
+}
